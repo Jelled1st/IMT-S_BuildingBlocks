d2a397863074ed76d095013aac8961dd9aa69e9e
Jelle Vrieze <jelle.vrieze@gmail.com>
2021-09-27 16:28:54 +0200

    [ADD] Macros replacements for UE_LOG

--- BuildingBlocks/Source/BuildingBlocks/Debug/Debug.h
+++ BuildingBlocks/Source/BuildingBlocks/Debug/Debug.h
@@ -4,9 +4,10 @@
 
 #include "CoreMinimal.h"
 
-/**
- * 
- */
+#define DebugLog(msg, ...)		UE_LOG(LogTemp, Log, TEXT(msg), ##__VA_ARGS__)
+#define DebugError(msg, ...)	UE_LOG(LogTemp, Error, TEXT(msg), ##__VA_ARGS__)
+#define DebugWarning(msg, ...)	UE_LOG(LogTemp, Warning, TEXT(msg), ##__VA_ARGS__)
+
 class BUILDINGBLOCKS_API Debug
 {
 public:
