43ecee2f70b9730bcc0f36697c1a1b5284aa8cfb
Jelle Vrieze <jelle.vrieze@gmail.com>
2021-09-27 16:57:13 +0200

    [EDIT] Moved debugging function implementation to cpp file

--- BuildingBlocks/Source/BuildingBlocks/Debug/Debug.h
+++ BuildingBlocks/Source/BuildingBlocks/Debug/Debug.h
@@ -14,23 +14,11 @@ public:
 	Debug();
 	~Debug();
 
-	static void Log(FString msg)
-	{
-		UE_LOG(LogTemp, Log, TEXT("%s"), *msg);
-	}
+	static void Log(FString msg);
 
-	static void Error(FString msg)
-	{
-		UE_LOG(LogTemp, Error, TEXT("%s"), *msg);
-	}
+	static void Error(FString msg);
 
-	static void Warning(FString msg)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("%s"), *msg);
-	}
+	static void Warning(FString msg);
 
-	static void ToScreen(FString msg, FColor color = FColor::White, float time = 5.0f, int32 key = -1, bool newerOnTop = true, FVector2D scale = FVector2D::UnitVector)
-	{
-		GEngine->AddOnScreenDebugMessage(key, time, color, msg, newerOnTop, scale);
-	}
+	static void ToScreen(FString msg, FColor color = FColor::White, float time = 5.0f, int32 key = -1, bool newerOnTop = true, FVector2D scale = FVector2D::UnitVector);
 };
