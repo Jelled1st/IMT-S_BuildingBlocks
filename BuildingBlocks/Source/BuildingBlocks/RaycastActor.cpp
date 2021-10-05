a5fad8fee9ce7b33c0325fdd7a34990a06c5c667
Laurentiu <laly_lepadat@yahoo.ro>
2021-10-05 20:25:42 +0300

    [ADD] Rudimentary UI & Unfinished raycast actor

---
+++ BuildingBlocks/Source/BuildingBlocks/RaycastActor.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RaycastActor.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+
+
+// Sets default values
+ARaycastActor::ARaycastActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	player=UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+
+	//Get input component from player to avoid memory acess violation
+	// https://answers.unrealengine.com/questions/218218/how-to-get-inputcomponent-in-c.html  ->possible solution
+	
+
+	//Memory acess violation, missing Uin
+	//player->InputComponent->BindAction("Raycast_Button", IE_Pressed, this, &ARaycastActor::RayCast);
+
+}
+
+// Called when the game starts or when spawned
+void ARaycastActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARaycastActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+void ARaycastActor::RayCast() {
+	FHitResult* hit = new FHitResult();
+	FVector forward = GetWorld()->GetFirstPlayerController()->PlayerCameraManager->GetActorForwardVector();
+	FVector start = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)->GetActorLocation();
+	start.Z = 100.f;
+	FVector end = (forward * 1000.f) + start;
+
+	if (GetWorld()->LineTraceSingleByChannel(*hit, start, end, ECC_WorldStatic)) {
+		DrawDebugLine(GetWorld(), start, end, FColor::Red, true);
+
+		if (hit->GetActor() != NULL) {
+			UE_LOG(LogClass, Log, TEXT("Component: %s"), *hit->GetComponent()->GetName());
+		}
+	}
+
+
+}
+
