d7e2d065756ddb7edc8442a3e80202651f32accf
Jelle Vrieze <jelle.vrieze@gmail.com>
2021-11-04 12:54:41 +0100

    [EDIT] Rename PlayerData to SportPlayer

--- BuildingBlocks/Source/BuildingBlocks/SportData/SportPlayer.cpp
+++ BuildingBlocks/Source/BuildingBlocks/SportData/SportPlayer.cpp
@@ -1,12 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "PlayerData.h"
+#include "SportPlayer.h"
 
-FPlayerData::FPlayerData()
+FSportPlayer::FSportPlayer()
 {
 }
 
-FPlayerData::~FPlayerData()
+FSportPlayer::~FSportPlayer()
 {
 }
--- BuildingBlocks/Source/BuildingBlocks/SportData/SportPlayer.h
+++ BuildingBlocks/Source/BuildingBlocks/SportData/SportPlayer.h
@@ -4,16 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "PlayerName.h"
-#include "PlayerData.generated.h"
+#include "Country.h"
+#include "SportPlayer.generated.h"
 
-USTRUCT(BluePrintType) struct FPlayerData
+// Is called SportPlayer since player is already a class in UE4
+USTRUCT(BluePrintType) struct FSportPlayer
 {
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite) FPlayerName playerName;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite) int length;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite) float weight;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) TEnumAsByte<Country> nationality;
 
-	FPlayerData();
-	~FPlayerData();
+	FSportPlayer();
+	~FSportPlayer();
 };
