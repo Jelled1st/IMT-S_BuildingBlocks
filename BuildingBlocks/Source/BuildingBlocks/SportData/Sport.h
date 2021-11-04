fbc710c847c6de4d3b65f6c18d17db6635141d6f
Jelle Vrieze <jelle.vrieze@gmail.com>
2021-11-04 14:59:01 +0100

    [ADD] Sport enum

---
+++ BuildingBlocks/Source/BuildingBlocks/SportData/Sport.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Country.generated.h"
+
+UENUM(BlueprintType)
+enum Sport
+{
+	Cricket,
+	Football,
+	Formula1,
+};
