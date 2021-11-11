3cf8c1a022f3f560b66a4837d0aa6c5f2a1cf482
Laurentiu <laly_lepadat@yahoo.ro>
2021-11-11 20:03:00 +0200

    [ADD] extended http object get methods
    Now you can get race tracks, drivers name and nationality of year 2020

--- BuildingBlocks/Source/BuildingBlocks/HTTPTestObject.cpp
+++ BuildingBlocks/Source/BuildingBlocks/HTTPTestObject.cpp
@@ -2,6 +2,7 @@
 
 
 #include "HTTPTestObject.h"
+#include "Kismet/KismetStringLibrary.h"
 #include "Runtime/Engine/Public/EngineGlobals.h"
 #include "Runtime/Online/HTTP/Public/Http.h"
 
@@ -13,7 +14,10 @@ UHTTPTestObject::UHTTPTestObject() {
 void UHTTPTestObject::Start()
 {
 	//TODO: public callable htttp get request
-	HttpGetRequest();
+	//HttpGetRequest();
+	//HttpGetRacersOf2020();
+	//HttpGetNationalitiesOf2020();
+	HttpGetRaceTracksOf2020();
 
 	
 }
@@ -25,6 +29,45 @@ void UHTTPTestObject::HttpGetRequest()
 
 	//process GET request
 	Request->SetURL("https://ergast.com/api/f1/1988.json");
+	Request->SetVerb("GET");
+	Request->SetHeader(TEXT("User-Agent"), "X-UnrealEngine-Agent");
+	Request->SetHeader("Content-Type", TEXT("application/json"));
+	Request->ProcessRequest();
+}
+
+void UHTTPTestObject::HttpGetRacersOf2020()
+{
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = httpModule->CreateRequest();
+	Request->OnProcessRequestComplete().BindUObject(this, &UHTTPTestObject::OnResponseReceivedRacers);
+
+	//process GET request
+	Request->SetURL("http://ergast.com/api/f1/2020/drivers.json");
+	Request->SetVerb("GET");
+	Request->SetHeader(TEXT("User-Agent"), "X-UnrealEngine-Agent");
+	Request->SetHeader("Content-Type", TEXT("application/json"));
+	Request->ProcessRequest();
+}
+
+void UHTTPTestObject::HttpGetNationalitiesOf2020()
+{
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = httpModule->CreateRequest();
+	Request->OnProcessRequestComplete().BindUObject(this, &UHTTPTestObject::OnResponseReceivedNationality);
+
+	//process GET request
+	Request->SetURL("http://ergast.com/api/f1/2020/drivers.json");
+	Request->SetVerb("GET");
+	Request->SetHeader(TEXT("User-Agent"), "X-UnrealEngine-Agent");
+	Request->SetHeader("Content-Type", TEXT("application/json"));
+	Request->ProcessRequest();
+}
+
+void UHTTPTestObject::HttpGetRaceTracksOf2020()
+{
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = httpModule->CreateRequest();
+	Request->OnProcessRequestComplete().BindUObject(this, &UHTTPTestObject::OnResponseReceivedTracks);
+
+	//process GET request
+	Request->SetURL("http://ergast.com/api/f1/2020/circuits.json");
 	Request->SetVerb("GET");
 	Request->SetHeader(TEXT("User-Agent"), "X-UnrealEngine-Agent");
 	Request->SetHeader("Content-Type", TEXT("application/json"));
@@ -54,5 +97,70 @@ void UHTTPTestObject::OnResponseReceived(FHttpRequestPtr request, FHttpResponseP
 		//TODO: integrate with IMgui
 		GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Green, recievedString);
 		//GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Green, FString::FromInt(recievedInt));
+	}
+}
+
+void UHTTPTestObject::OnResponseReceivedRacers(FHttpRequestPtr request, FHttpResponsePtr response, bool isSuccessful)
+{	
+	TSharedPtr<FJsonObject> JsonObject;
+	TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(response->GetContentAsString());
+
+	if (FJsonSerializer::Deserialize(Reader, JsonObject))
+	{
+
+		FString stringOfRacers= JsonObject->GetObjectField("MRData")->GetStringField("total");
+		int32 nrOfRacers = UKismetStringLibrary::Conv_StringToInt(stringOfRacers);
+
+		for (int i = 0; i < nrOfRacers; i++) {
+			FString firstName = JsonObject->GetObjectField("MRData")->GetObjectField("DriverTable")->GetArrayField("Drivers")[i]->AsObject()->GetStringField("givenName");
+			FString lastName = JsonObject->GetObjectField("MRData")->GetObjectField("DriverTable")->GetArrayField("Drivers")[i]->AsObject()->GetStringField("familyName");
+			FString fullName = firstName + "_" + lastName;
+			racersNameOf2020.Add(fullName);
+		}
+
+		//get all names stored in array of racers, if correct it should have the same number as the get request. As of now verifiable only in postman
+		GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Green, FString::FromInt( racersNameOf2020.Num()));
+	}
+}
+
+void UHTTPTestObject::OnResponseReceivedNationality(FHttpRequestPtr request, FHttpResponsePtr response, bool isSuccessful)
+{
+	TSharedPtr<FJsonObject> JsonObject;
+	TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(response->GetContentAsString());
+
+	if (FJsonSerializer::Deserialize(Reader, JsonObject))
+	{
+
+		FString stringOfRacers = JsonObject->GetObjectField("MRData")->GetStringField("total");
+		int32 nrOfRacers = UKismetStringLibrary::Conv_StringToInt(stringOfRacers);
+
+		for (int i = 0; i < nrOfRacers; i++) {
+			FString nationality= JsonObject->GetObjectField("MRData")->GetObjectField("DriverTable")->GetArrayField("Drivers")[i]->AsObject()->GetStringField("nationality");
+			nationalitiesOf2020.Add(nationality);
+		}
+
+		//get all nationalities stored in array of nationalities, if correct it should have the same number as the get request. As of now verifiable only in postman
+		GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Green, FString::FromInt(nationalitiesOf2020.Num()));
+	}
+}
+
+void UHTTPTestObject::OnResponseReceivedTracks(FHttpRequestPtr request, FHttpResponsePtr response, bool isSuccessful)
+{
+	TSharedPtr<FJsonObject> JsonObject;
+	TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(response->GetContentAsString());
+
+	if (FJsonSerializer::Deserialize(Reader, JsonObject))
+	{
+
+		FString stringOfTracks = JsonObject->GetObjectField("MRData")->GetStringField("total");
+		int32 nrOfTracks = UKismetStringLibrary::Conv_StringToInt(stringOfTracks);
+
+		for (int i = 0; i < nrOfTracks; i++) {
+			FString nationality = JsonObject->GetObjectField("MRData")->GetObjectField("CircuitTable")->GetArrayField("Circuits")[i]->AsObject()->GetStringField("circuitName");
+			racerTracksOf2020.Add(nationality);
+		}
+
+		//get all nationalities stored in array of nationalities, if correct it should have the same number as the get request. As of now verifiable only in postman
+		GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Green, FString::FromInt(racerTracksOf2020.Num()));
 	}
 }
--- BuildingBlocks/Source/BuildingBlocks/HTTPTestObject.h
+++ BuildingBlocks/Source/BuildingBlocks/HTTPTestObject.h
@@ -24,9 +24,25 @@ public:
 
 
 	UFUNCTION()
+		//TODO: add bindable object and url to get request in order to minimize amount of get requests
 		void HttpGetRequest();
+		void HttpGetRacersOf2020();
+		void HttpGetNationalitiesOf2020();
+		void HttpGetRaceTracksOf2020();
 
+
+	//TODO: condeness responses in one unqie response based on url?
 	void OnResponseReceived(FHttpRequestPtr request, FHttpResponsePtr response, bool isSuccessful);
+	void OnResponseReceivedRacers(FHttpRequestPtr request, FHttpResponsePtr response, bool isSuccessful);
+	void OnResponseReceivedNationality(FHttpRequestPtr request, FHttpResponsePtr response, bool isSuccessful);
+	void OnResponseReceivedTracks(FHttpRequestPtr request, FHttpResponsePtr response, bool isSuccessful);
+
+
+
+	//these are to be replaced by player and team objects
+	TArray<FString> racersNameOf2020;
+	TArray<FString> nationalitiesOf2020;
+	TArray<FString> racerTracksOf2020;
 
 	//TODO public list of players, race tracks, nationalities, teams. To be replaced by players & team classes in fututre iteration
 };
