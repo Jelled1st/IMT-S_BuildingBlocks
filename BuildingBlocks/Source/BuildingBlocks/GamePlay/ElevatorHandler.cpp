e716e7253af0edad0997be99dd7bcb111efa6c21
Jelle Vrieze <jelle.vrieze@gmail.com>
2021-11-25 14:06:43 +0100

    [EDIT] Switched the include order

--- BuildingBlocks/Source/BuildingBlocks/GamePlay/ElevatorHandler.cpp
+++ BuildingBlocks/Source/BuildingBlocks/GamePlay/ElevatorHandler.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Elevator.h"
 #include "ElevatorHandler.h"
+#include "Elevator.h"
 
 void UElevatorHandler::Register(AElevator& evelator)
 {
